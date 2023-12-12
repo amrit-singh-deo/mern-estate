7a1c1719e3542d283a088813e65c2c2fcbb8ba17
Amrit Singh <amritsinghdeo.20@gmail.com>
2023-12-12 20:39:23 +0530

    connect to mognodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB . . . ");
+  })
+  .catch((err) => {
+    console.log(err + "\nCannot connect to MongoDB! . . .");
+  });
+
 const app = express();
-app.listen(3000, ()=>{
-    console.log("Server is listening to 3000 . . .")
-})
+app.listen(3000, () => {
+  console.log("Server is listening to 3000 . . .");
+});
